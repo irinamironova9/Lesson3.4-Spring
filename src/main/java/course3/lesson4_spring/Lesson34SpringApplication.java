a452bb51adf31cdc7c30cfe38233f081128bf0ad
Ирина <mi.irene97@gmail.com>
2022-11-23 19:15:53 +0300

    Initial commit

---
+++ src/main/java/course3/lesson4_spring/Lesson34SpringApplication.java
@@ -0,0 +1,13 @@
+package course3.lesson4_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lesson34SpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lesson34SpringApplication.class, args);
+    }
+
+}
